e9e81b4bb9b44dd10a91b29601b0912ed132654d
葉大炮 <moyamoya@mail.shmps.kh.edu.tw>
2018-12-30 16:27:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,23 @@ function checkUser{
 
 
 }
-
-
-
-
 //menu,包含消耗品管理、教師清單管理、各月份報表匯出
 function menu{
 
 
 }
+
+/*
+這邊是管理消耗品的頁面
+*/
+
+//item_add_form
+//item_update_form
+//item_add
+//item_update
+//item_showList
+
+/*
+這邊是教師清單的管理頁面
+*/
 ?>
